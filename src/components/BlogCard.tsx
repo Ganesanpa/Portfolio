d9ef2e212c2f94bc3f969667aa431dc1c3fdd275
Ganesanpa <ganesanpa03@gmail.com>
2025-08-18 21:27:01 +0530

    modify ode

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+
 interface BlogCardProps {
   id: string;
   title: string;
@@ -9,12 +9,7 @@ export default function BlogCard({ id, title, summary }: BlogCardProps) {
     <div className="border rounded-2xl p-4 shadow-md hover:shadow-xl transition">
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-muted-foreground mb-4">{summary}</p>
-      <Link
-        href={`/blog/${id}`}
-        className="text-blue-500 hover:underline text-sm font-medium"
-      >
-        Read more →
-      </Link>
+      
     </div>
   );
 }
